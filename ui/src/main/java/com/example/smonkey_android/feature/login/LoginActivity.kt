ac8856a03930b088933b2a1b75c0be9415bb5c83
유현명 <aahspringaa4@naver.com>
2022-10-12 19:39:25 +0900

    ⚡️ :: New Activity & Screen

---
+++ ui/src/main/java/com/example/smonkey_android/feature/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.smonkey_android.feature.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.smonkey_android.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
