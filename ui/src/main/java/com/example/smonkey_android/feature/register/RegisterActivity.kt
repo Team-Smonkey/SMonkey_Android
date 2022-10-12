ecca5be529fd314c170795e483035c74ff9177cf
유현명 <aahspringaa4@naver.com>
2022-10-12 23:16:06 +0900

    ⚡️ :: Register View & Activity

---
+++ ui/src/main/java/com/example/smonkey_android/feature/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.smonkey_android.feature.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.smonkey_android.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
