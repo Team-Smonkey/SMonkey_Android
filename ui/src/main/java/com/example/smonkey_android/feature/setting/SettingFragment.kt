ab6e72387f44831bad94eee73b7f15c84842e046
유현명 <aahspringaa4@naver.com>
2022-10-31 20:51:39 +0900

    ⚡️ :: Setting Fragment

---
+++ ui/src/main/java/com/example/smonkey_android/feature/setting/SettingFragment.kt
@@ -0,0 +1,13 @@
+package com.example.smonkey_android.feature.setting
+
+import com.example.smonkey_android.R
+import com.example.smonkey_android.base.BaseFragment
+import com.example.smonkey_android.databinding.FragmentSettingBinding
+
+class SettingFragment : BaseFragment<FragmentSettingBinding>(
+    R.layout.fragment_setting
+) {
+    override fun initView() {
+
+    }
+}
