518902298b8d8cc588acacc518ea434ff9df0259
유현명 <aahspringaa4@naver.com>
2022-10-31 20:50:08 +0900

    ⚡️ :: LoginActivity

--- ui/src/main/java/com/example/smonkey_android/feature/login/LoginActivity.kt
+++ ui/src/main/java/com/example/smonkey_android/feature/login/LoginActivity.kt
@@ -1,12 +1,47 @@
 package com.example.smonkey_android.feature.login
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import com.example.smonkey_android.R
+import com.example.smonkey_android.base.BaseActivity
+import com.example.smonkey_android.databinding.ActivityLoginBinding
+import com.example.smonkey_android.feature.home.HomeActivity
+import com.example.smonkey_android.feature.register.RegisterActivity
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity<ActivityLoginBinding>(
+    R.layout.activity_login
+) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+    }
+
+    override fun initView() {
+        binding.register.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btLogin.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            finishAffinity()
+            startActivity(intent)
+        }
+
+        binding.constraint.setOnClickListener {
+            hideKeyboard()
+        }
+    }
+
+    @SuppressLint("ServiceCast")
+    private fun hideKeyboard() {
+        val inputManager: InputMethodManager =
+            this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.hideSoftInputFromWindow(
+            this.currentFocus!!.windowToken,
+            InputMethodManager.HIDE_NOT_ALWAYS
+        )
     }
 }
