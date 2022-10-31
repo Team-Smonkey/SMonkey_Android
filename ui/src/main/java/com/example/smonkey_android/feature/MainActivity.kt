46cf4d20978bd0d175f5660dfd152be5e2243d9f
유현명 <aahspringaa4@naver.com>
2022-10-31 20:50:50 +0900

    ⚡️ :: MainActivity

--- ui/src/main/java/com/example/smonkey_android/feature/MainActivity.kt
+++ ui/src/main/java/com/example/smonkey_android/feature/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.example.smonkey_android.feature
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.smonkey_android.R
+import com.example.smonkey_android.base.BaseActivity
+import com.example.smonkey_android.databinding.ActivityMainBinding
+import com.example.smonkey_android.feature.login.LoginActivity
+import com.example.smonkey_android.feature.register.RegisterActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(
+    R.layout.activity_main
+) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+    }
+
+    override fun initView() {
+        binding.btLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
