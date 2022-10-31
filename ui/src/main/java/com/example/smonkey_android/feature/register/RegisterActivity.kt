9d5d312aea46215200f427a5d235a692f83f37fc
유현명 <aahspringaa4@naver.com>
2022-10-31 20:51:17 +0900

    ⚡️ :: RegisterActivity

--- ui/src/main/java/com/example/smonkey_android/feature/register/RegisterActivity.kt
+++ ui/src/main/java/com/example/smonkey_android/feature/register/RegisterActivity.kt
@@ -1,12 +1,34 @@
 package com.example.smonkey_android.feature.register
 
+import android.annotation.SuppressLint
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import com.example.smonkey_android.R
+import com.example.smonkey_android.base.BaseActivity
+import com.example.smonkey_android.databinding.ActivityRegisterBinding
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : BaseActivity<ActivityRegisterBinding>(
+    R.layout.activity_register
+) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+    }
+
+    override fun initView() {
+        binding.constraint.setOnClickListener {
+            hideKeyboard()
+        }
+    }
+
+    @SuppressLint("ServiceCast")
+    private fun hideKeyboard() {
+        val inputManager: InputMethodManager =
+            this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.hideSoftInputFromWindow(
+            this.currentFocus!!.windowToken,
+            InputMethodManager.HIDE_NOT_ALWAYS
+        )
     }
 }
