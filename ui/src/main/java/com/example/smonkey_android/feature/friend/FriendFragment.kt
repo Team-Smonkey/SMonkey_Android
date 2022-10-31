8beed222f366bc40a18ad6889777370575672b36
유현명 <aahspringaa4@naver.com>
2022-10-31 20:49:10 +0900

    ⚡️ :: FriendFragment

---
+++ ui/src/main/java/com/example/smonkey_android/feature/friend/FriendFragment.kt
@@ -0,0 +1,13 @@
+package com.example.smonkey_android.feature.friend
+
+import com.example.smonkey_android.R
+import com.example.smonkey_android.base.BaseFragment
+import com.example.smonkey_android.databinding.FragmentFriendBinding
+
+class FriendFragment : BaseFragment<FragmentFriendBinding>(
+    R.layout.fragment_friend
+) {
+    override fun initView() {
+
+    }
+}
