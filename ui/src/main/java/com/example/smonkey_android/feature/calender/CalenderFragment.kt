c00cd5b648cb6b749f9edd21ed1c09f25c07cadb
유현명 <aahspringaa4@naver.com>
2022-10-31 20:48:35 +0900

    ⚡️ :: CalenderFragment

---
+++ ui/src/main/java/com/example/smonkey_android/feature/calender/CalenderFragment.kt
@@ -0,0 +1,13 @@
+package com.example.smonkey_android.feature.calender
+
+import com.example.smonkey_android.R
+import com.example.smonkey_android.base.BaseFragment
+import com.example.smonkey_android.databinding.FragmentCalenderBinding
+
+class CalenderFragment : BaseFragment<FragmentCalenderBinding>(
+    R.layout.fragment_calender
+) {
+    override fun initView() {
+
+    }
+}
