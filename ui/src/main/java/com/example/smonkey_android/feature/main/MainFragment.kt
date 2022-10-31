484ab71a543157e731eb9cede4a64ffe5a5d5d10
유현명 <aahspringaa4@naver.com>
2022-10-31 20:50:33 +0900

    ⚡️ :: Main Fragment

---
+++ ui/src/main/java/com/example/smonkey_android/feature/main/MainFragment.kt
@@ -0,0 +1,13 @@
+package com.example.smonkey_android.feature.main
+
+import com.example.smonkey_android.R
+import com.example.smonkey_android.base.BaseFragment
+import com.example.smonkey_android.databinding.FragmentHomeBinding
+
+class MainFragment : BaseFragment<FragmentHomeBinding>(
+    R.layout.fragment_home
+) {
+    override fun initView() {
+
+    }
+}
