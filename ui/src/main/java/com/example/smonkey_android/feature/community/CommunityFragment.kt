e555a6ddccc388d4036f1bc9b51b2372d83f4a8c
유현명 <aahspringaa4@naver.com>
2022-10-31 20:48:50 +0900

    ⚡️ :: CommunityFragment

---
+++ ui/src/main/java/com/example/smonkey_android/feature/community/CommunityFragment.kt
@@ -0,0 +1,13 @@
+package com.example.smonkey_android.feature.community
+
+import com.example.smonkey_android.R
+import com.example.smonkey_android.base.BaseFragment
+import com.example.smonkey_android.databinding.FragmentCommunityBinding
+
+class CommunityFragment : BaseFragment<FragmentCommunityBinding>(
+    R.layout.fragment_community
+) {
+    override fun initView() {
+
+    }
+}
