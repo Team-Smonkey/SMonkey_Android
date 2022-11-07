b654edb6fdb0ef559e5bce3188bddcc17687d211
유현명 <aahspringaa4@naver.com>
2022-11-07 17:03:57 +0900

    ⚡️ :: MonkeyColorActvity

---
+++ ui/src/main/java/com/example/smonkey_android/feature/monkeycolor/MonkeyColorActivity.kt
@@ -0,0 +1,19 @@
+package com.example.smonkey_android.feature.monkeycolor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.smonkey_android.R
+import com.example.smonkey_android.base.BaseActivity
+import com.example.smonkey_android.databinding.ActivityMonkeyColorBinding
+
+class MonkeyColorActivity : BaseActivity<ActivityMonkeyColorBinding>(
+    R.layout.activity_monkey_color
+) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun initView() {
+
+    }
+}
