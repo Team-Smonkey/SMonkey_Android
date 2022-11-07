b697bcdb20f3bcb9b8aa7c4b35403999b7c2b8e9
유현명 <aahspringaa4@naver.com>
2022-11-07 17:03:13 +0900

    ⚡️ :: Friend View

--- ui/src/main/java/com/example/smonkey_android/feature/friend/FriendFragment.kt
+++ ui/src/main/java/com/example/smonkey_android/feature/friend/FriendFragment.kt
@@ -1,5 +1,6 @@
 package com.example.smonkey_android.feature.friend
 
+import android.view.View
 import com.example.smonkey_android.R
 import com.example.smonkey_android.base.BaseFragment
 import com.example.smonkey_android.databinding.FragmentFriendBinding
@@ -9,5 +10,51 @@ class FriendFragment : BaseFragment<FragmentFriendBinding>(
 ) {
     override fun initView() {
 
+        setFrag(0)
+
+        binding.btFriendList.setOnClickListener{
+            setFrag(0)
+        }
+
+        binding.btFindFriend.setOnClickListener {
+            setFrag(1)
+        }
+
+        binding.btRequestFreiend.setOnClickListener {
+            setFrag(2)
+        }
+
+        binding.btCheer.setOnClickListener {
+            setFrag(3)
+        }
+    }
+
+    private fun setFrag(fragNum: Int) {
+        when (fragNum) {
+            0 -> {
+                binding.friendlist.visibility = View.VISIBLE
+                binding.findFriend.visibility = View.INVISIBLE
+                binding.requestFreiend.visibility = View.INVISIBLE
+                binding.cheer.visibility = View.INVISIBLE
+            }
+            1 -> {
+                binding.friendlist.visibility = View.INVISIBLE
+                binding.findFriend.visibility = View.VISIBLE
+                binding.requestFreiend.visibility = View.INVISIBLE
+                binding.cheer.visibility = View.INVISIBLE
+            }
+            2 -> {
+                binding.friendlist.visibility = View.INVISIBLE
+                binding.findFriend.visibility = View.INVISIBLE
+                binding.requestFreiend.visibility = View.VISIBLE
+                binding.cheer.visibility = View.INVISIBLE
+            }
+            3 -> {
+                binding.friendlist.visibility = View.INVISIBLE
+                binding.findFriend.visibility = View.INVISIBLE
+                binding.requestFreiend.visibility = View.INVISIBLE
+                binding.cheer.visibility = View.VISIBLE
+            }
+        }
     }
 }
