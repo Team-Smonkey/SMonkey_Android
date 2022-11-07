2bd7e00b8827a76a688ac4e0751763b3576ea2e3
유현명 <aahspringaa4@naver.com>
2022-11-07 17:02:01 +0900

    ⚡️ :: ChangeId Activity XML

---
+++ ui/src/main/java/com/example/smonkey_android/feature/editauth/ChangeIdActivity.kt
@@ -0,0 +1,19 @@
+package com.example.smonkey_android.feature.editauth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.smonkey_android.R
+import com.example.smonkey_android.base.BaseActivity
+import com.example.smonkey_android.databinding.ActivityChangeIdBinding
+
+class ChangeIdActivity : BaseActivity<ActivityChangeIdBinding>(
+    R.layout.activity_change_id
+) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun initView() {
+
+    }
+}
