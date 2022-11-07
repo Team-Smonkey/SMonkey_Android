582b2f5d8355cd76fb3310ff354e83b22cc67fb9
유현명 <aahspringaa4@naver.com>
2022-11-07 17:03:29 +0900

    ⚡️ :: HealthActivity

---
+++ ui/src/main/java/com/example/smonkey_android/feature/health/HealthActivity.kt
@@ -0,0 +1,19 @@
+package com.example.smonkey_android.feature.health
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.smonkey_android.R
+import com.example.smonkey_android.base.BaseActivity
+import com.example.smonkey_android.databinding.ActivityHealthBinding
+
+class HealthActivity : BaseActivity<ActivityHealthBinding>(
+    R.layout.activity_health
+) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun initView() {
+
+    }
+}
