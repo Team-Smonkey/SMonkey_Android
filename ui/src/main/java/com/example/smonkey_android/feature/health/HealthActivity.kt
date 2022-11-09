097596c21a115103bd88784a3052bbb733dff34e
유현명 <aahspringaa4@naver.com>
2022-11-09 15:51:09 +0900

    ⚡️ :: Scan Smoke Information Activity

--- ui/src/main/java/com/example/smonkey_android/feature/health/HealthActivity.kt
+++ ui/src/main/java/com/example/smonkey_android/feature/health/HealthActivity.kt
@@ -1,10 +1,12 @@
 package com.example.smonkey_android.feature.health
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import android.os.Bundle
 import com.example.smonkey_android.R
 import com.example.smonkey_android.base.BaseActivity
 import com.example.smonkey_android.databinding.ActivityHealthBinding
+import java.util.Calendar
 
 class HealthActivity : BaseActivity<ActivityHealthBinding>(
     R.layout.activity_health
@@ -14,6 +16,42 @@ class HealthActivity : BaseActivity<ActivityHealthBinding>(
     }
 
     override fun initView() {
+        binding.view3.setOnClickListener {
+            val c = Calendar.getInstance()
+            val nYear = c[Calendar.YEAR]
+            val nMon = c[Calendar.MONTH]
+            val nDay = c[Calendar.DAY_OF_MONTH]
 
+            val mDateSetListener =
+                OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                    binding.tvSmokingDay.text = year.toString() + "년 " + (monthOfYear + 1).toString() + "월 " + dayOfMonth.toString() + "일"
+                }
+
+            val oDialog = DatePickerDialog(
+                this@HealthActivity,
+                android.R.style.Theme_DeviceDefault_Light_Dialog,
+                mDateSetListener, nYear, nMon, nDay
+            )
+            oDialog.show()
+        }
+
+        binding.view4.setOnClickListener {
+            val c = Calendar.getInstance()
+            val nYear = c[Calendar.YEAR]
+            val nMon = c[Calendar.MONTH]
+            val nDay = c[Calendar.DAY_OF_MONTH]
+
+            val mDateSetListener =
+                OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                    binding.tvNonSmokingTime.text = year.toString() + "년 " + (monthOfYear + 1).toString() + "월 " + dayOfMonth.toString() + "일"
+                }
+
+            val oDialog = DatePickerDialog(
+                this@HealthActivity,
+                android.R.style.Theme_DeviceDefault_Light_Dialog,
+                mDateSetListener, nYear, nMon, nDay
+            )
+            oDialog.show()
+        }
     }
 }
