5c9362b0b2c5b6bfe37abb6fa50cd76dd515b779
유현명 <aahspringaa4@naver.com>
2022-11-14 20:23:12 +0900

    ⚡️ :: RemoteCommunityDataSource

---
+++ data/src/main/java/com/example/data/remote/datasource/declaration/RemoteCommunityDataSource.kt
@@ -0,0 +1,27 @@
+package com.example.data.remote.datasource.declaration
+
+import com.example.data.domain.enums.CategoryType
+import com.example.data.remote.request.community.CreateCommunityRequest
+import com.example.data.remote.request.community.UpdateCommunityRequest
+import com.example.data.remote.response.community.FetchCommunityDetailResponse
+import com.example.data.remote.response.community.FetchCommunityResponse
+import io.reactivex.Completable
+import io.reactivex.Single
+import java.util.UUID
+
+interface RemoteCommunityDataSource {
+
+    fun createCommunity(createCommunityRequest: CreateCommunityRequest): Single<Completable>
+
+    fun deleteCommunity(id: Int): Single<Completable>
+
+    fun updateCommunity(updateCommunityRequest: UpdateCommunityRequest): Single<Completable>
+
+    fun fetchCommunity(category: CategoryType): Single<FetchCommunityResponse>
+
+    fun fetchCommunityDetail(feedId: UUID): Single<FetchCommunityDetailResponse>
+
+    fun feedLike(feedId: UUID): Single<Completable>
+
+    fun feedUnLike(feedId: UUID): Single<Completable>
+}
---
+++ data/src/main/java/com/example/data/remote/datasource/implementation/RemoteCommunityDataSourceImpl.kt
@@ -0,0 +1,56 @@
+package com.example.data.remote.datasource.implementation
+
+import com.example.data.domain.enums.CategoryType
+import com.example.data.domain.token.ACCESS_TOKEN
+import com.example.data.remote.api.CommunityApi
+import com.example.data.remote.datasource.declaration.RemoteCommunityDataSource
+import com.example.data.remote.request.community.CreateCommunityRequest
+import com.example.data.remote.request.community.UpdateCommunityRequest
+import com.example.data.remote.response.community.FetchCommunityDetailResponse
+import com.example.data.remote.response.community.FetchCommunityResponse
+import io.reactivex.Completable
+import io.reactivex.Single
+import java.util.UUID
+
+class RemoteCommunityDataSourceImpl(
+    private val communityApi: CommunityApi
+): RemoteCommunityDataSource {
+    override fun createCommunity(createCommunityRequest: CreateCommunityRequest): Single<Completable> =
+        communityApi.createCommunity(createCommunityRequest)
+
+    override fun deleteCommunity(id: Int): Single<Completable> =
+        communityApi.deleteCommunity(
+            accessToken = ACCESS_TOKEN,
+            id
+        )
+
+    override fun updateCommunity(updateCommunityRequest: UpdateCommunityRequest): Single<Completable> =
+        communityApi.updateCommunity(
+            accessToken = ACCESS_TOKEN,
+            updateCommunityRequest
+        )
+
+    override fun fetchCommunity(category: CategoryType): Single<FetchCommunityResponse> =
+        communityApi.fetchCommunity(
+            accessToken = ACCESS_TOKEN,
+            category
+        )
+
+    override fun fetchCommunityDetail(feedId: UUID): Single<FetchCommunityDetailResponse> =
+        communityApi.fetchCommunityDetail(
+            accessToken = ACCESS_TOKEN,
+            feedId
+        )
+
+    override fun feedLike(feedId: UUID): Single<Completable> =
+        communityApi.feedLike(
+            accessToken = ACCESS_TOKEN,
+            feedId
+        )
+
+    override fun feedUnLike(feedId: UUID): Single<Completable> =
+        communityApi.feedUnLike(
+            accessToken = ACCESS_TOKEN,
+            feedId
+        )
+}
