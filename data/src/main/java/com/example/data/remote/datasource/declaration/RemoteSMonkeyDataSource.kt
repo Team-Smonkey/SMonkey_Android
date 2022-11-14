48d1c8158e4d8a40561ec817c98ab9c576f30072
유현명 <aahspringaa4@naver.com>
2022-11-14 20:23:00 +0900

    ⚡️ :: RemoteSMonkeyDataSource

---
+++ data/src/main/java/com/example/data/remote/datasource/declaration/RemoteSMonkeyDataSource.kt
@@ -0,0 +1,15 @@
+package com.example.data.remote.datasource.declaration
+
+import com.example.data.remote.request.smonkey.MakeSMonkeyRequest
+import com.example.data.remote.response.smonkey.SearchSMonkeyResponse
+import io.reactivex.Completable
+import io.reactivex.Single
+
+interface RemoteSMonkeyDataSource {
+
+    fun makeSMonkey(makeSMonkeyRequest: MakeSMonkeyRequest):Single<Completable>
+
+    fun alterSMonkeyColor(color: String): Single<Completable>
+
+    fun searchSMonkey(accessToken: String): Single<SearchSMonkeyResponse>
+}
---
+++ data/src/main/java/com/example/data/remote/datasource/implementation/RemoteSMonkeyDataSourceImpl.kt
@@ -0,0 +1,26 @@
+package com.example.data.remote.datasource.implementation
+
+import com.example.data.domain.token.ACCESS_TOKEN
+import com.example.data.remote.api.SMonkeyApi
+import com.example.data.remote.datasource.declaration.RemoteSMonkeyDataSource
+import com.example.data.remote.request.smonkey.MakeSMonkeyRequest
+import com.example.data.remote.response.smonkey.SearchSMonkeyResponse
+import io.reactivex.Completable
+import io.reactivex.Single
+
+class RemoteSMonkeyDataSourceImpl(
+    private val sMonkeyApi: SMonkeyApi
+): RemoteSMonkeyDataSource {
+    override fun makeSMonkey(makeSMonkeyRequest: MakeSMonkeyRequest): Single<Completable> =
+        sMonkeyApi.makeSMonkey(makeSMonkeyRequest)
+
+
+    override fun alterSMonkeyColor(color: String): Single<Completable> =
+        sMonkeyApi.alterSMonkeyColor(
+            accessToken = ACCESS_TOKEN,
+            color = color
+        )
+
+    override fun searchSMonkey(accessToken: String): Single<SearchSMonkeyResponse> =
+        sMonkeyApi.searchSMonkey(accessToken)
+}
