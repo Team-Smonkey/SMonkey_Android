71f7196ebc676c6bc2611d39eded903796e5bbbd
유현명 <aahspringaa4@naver.com>
2022-11-16 22:01:17 +0900

    ⚡️ :: Promise Activity

--- ui/src/main/java/com/example/smonkey_android/feature/promise/PromiseActivity.kt
+++ ui/src/main/java/com/example/smonkey_android/feature/promise/PromiseActivity.kt
@@ -1,12 +1,25 @@
 package com.example.smonkey_android.feature.promise
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.smonkey_android.R
+import com.example.smonkey_android.base.BaseActivity
+import com.example.smonkey_android.databinding.ActivityPromiseBinding
+import com.example.smonkey_android.feature.health.HealthActivity
 
-class PromiseActivity : AppCompatActivity() {
+class PromiseActivity() : BaseActivity<ActivityPromiseBinding>(
+    R.layout.activity_promise
+) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_promise)
+
+    }
+
+    override fun initView() {
+        binding.btCheck.setOnClickListener {
+            val intent = Intent(this, HealthActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
