b74b922a11f9016374d469dffb6ede2181167599
유현명 <aahspringaa4@naver.com>
2022-11-16 21:48:40 +0900

    ⚡️ :: FriendApi Interface

---
+++ data/src/main/java/com/example/data/remote/api/FriendApi.kt
@@ -0,0 +1,49 @@
+package com.example.data.remote.api
+
+import com.example.data.remote.response.friend.FriendListResponse
+import com.example.data.remote.response.friend.RequestListResponse
+import com.example.data.remote.url.SMonkeyUrl
+import io.reactivex.Completable
+import io.reactivex.Single
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface FriendApi {
+
+    @POST(SMonkeyUrl.Friend.PathFriend)
+    fun addFriend(
+        @Header("Authorization") accessToken: String,
+        @Path("friend-id") friend_id: String,
+    ): Single<Completable>
+
+    @GET(SMonkeyUrl.Friends)
+    fun fetchFriendList(
+        @Header("Authorization") accessToken: String,
+    ): Single<FriendListResponse>
+
+    @GET(SMonkeyUrl.Friend.FetchFriend)
+    fun searchFriend(
+        @Header("Authorization") accessToken: String,
+        @Body name: String,
+    ): Single<Completable>
+
+    @POST(SMonkeyUrl.Friend.ReceiveFriendRequest)
+    fun receiveFriend(
+        @Header("Authorization") accessToken: String,
+        @Body sender_id: String,
+    ): Single<Completable>
+
+    @POST(SMonkeyUrl.Friend.RefuseFriendRequest)
+    fun refuseFriend(
+        @Header("Authorization") accessToken: String,
+        @Body sender_id: String,
+    ): Single<Completable>
+
+    @GET(SMonkeyUrl.Friend.RequestList)
+    fun fetchRequestList(
+        @Header("Authorization") accessToken: String,
+    ): Single<RequestListResponse>
+}
