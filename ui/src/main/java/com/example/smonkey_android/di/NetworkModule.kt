9b92ff97b10fe244b2d94d80f5db44e009e8f424
유현명 <aahspringaa4@naver.com>
2022-11-16 21:59:13 +0900

    ⚡️ :: Network DI Module

---
+++ ui/src/main/java/com/example/smonkey_android/di/NetworkModule.kt
@@ -0,0 +1,33 @@
+package com.example.smonkey_android.di
+
+import com.example.smonkey_android.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+val networkModule = module {
+    single {
+        HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        }
+    }
+
+    single {
+        Retrofit.Builder()
+            .baseUrl("http://smonkey.saehyun.kr")
+            .client(
+                OkHttpClient.Builder()
+                    .addInterceptor(get<HttpLoggingInterceptor>())
+                    .build())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
