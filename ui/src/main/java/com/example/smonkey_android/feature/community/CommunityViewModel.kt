9238b31e05c2e550891467cb42905b10f1e9b4f9
유현명 <aahspringaa4@naver.com>
2022-11-16 21:59:47 +0900

    ⚡️ :: CommunityViewModel

---
+++ ui/src/main/java/com/example/smonkey_android/feature/community/CommunityViewModel.kt
@@ -0,0 +1,51 @@
+package com.example.smonkey_android.feature.community
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.data.domain.enums.CategoryType
+import com.example.data.domain.usecase.community.CreateCommunityUseCase
+import com.example.data.domain.usecase.community.DeleteCommunityUseCase
+import com.example.data.domain.usecase.community.FetchCommunityUseCase
+import com.example.data.domain.usecase.community.UpdateCommunityUseCae
+import com.example.data.remote.response.community.FetchCommunityResponse
+import com.example.smonkey_android.util.MutableEventFlow
+import com.example.smonkey_android.util.asEventFlow
+import kotlinx.coroutines.launch
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.android.schedulers.AndroidSchedulers
+
+class CommunityViewModel(
+    private val createCommunityUseCase: CreateCommunityUseCase,
+    private val deleteCommunityUseCase: DeleteCommunityUseCase,
+    private val fetchCommunityUseCase: FetchCommunityUseCase,
+    private val updateCommunityUseCae: UpdateCommunityUseCae,
+) : ViewModel() {
+
+    private val _eventFlow = MutableEventFlow<Event>()
+    val eventFlow = _eventFlow.asEventFlow()
+
+    fun fetchCommunity(Category: CategoryType) {
+        fetchCommunityUseCase.execute(
+            Category, object : DisposableSingleObserver<List<FetchCommunityResponse>>() {
+                override fun onSuccess(t: List<FetchCommunityResponse>) {
+                    event(Event.FetchCommunity(t))
+                }
+
+                override fun onError(e: Throwable) {
+                    event(Event.ErrorMessage("게시물을 발생하던 도중 문제가 발생하였습니다."))
+                }
+            }, AndroidSchedulers.mainThread()
+        )
+    }
+
+    private fun event(event: Event) {
+        viewModelScope.launch {
+            _eventFlow.emit(event)
+        }
+    }
+
+    sealed class Event {
+        data class ErrorMessage(val message: String) : Event()
+        data class FetchCommunity(val fetchCommunityListResponse: List<FetchCommunityResponse>) : Event()
+    }
+}
