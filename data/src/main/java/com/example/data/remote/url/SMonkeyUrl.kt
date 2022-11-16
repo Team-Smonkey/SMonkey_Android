71a2ab58496ea42d1fd3dc2da3d5e9eb79164b71
유현명 <aahspringaa4@naver.com>
2022-11-16 21:57:31 +0900

    ⚡️ :: SMonkeyUrl

--- data/src/main/java/com/example/data/remote/url/SMonkeyUrl.kt
+++ data/src/main/java/com/example/data/remote/url/SMonkeyUrl.kt
@@ -4,6 +4,9 @@ object SMonkeyUrl {
 
     const val SMonkey = "/smonkey"
     const val Community = "/feed"
+    const val Friends = "/friend"
+    const val User = "/user"
+    const val Login = "/user/signIn"
 
     object SMonkeies {
         const val SMonkeyColor = "/smonkey/color"
@@ -13,5 +16,13 @@ object SMonkeyUrl {
     object Communities {
         const val CommunityDetail = "/feed/{feed-id}"
         const val CommunityLike = "${CommunityDetail}/like"
+    }
+
+    object Friend {
+        const val PathFriend = "${Friends}/{friend-id}"
+        const val FetchFriend = "${Friends}/search"
+        const val ReceiveFriendRequest = "${Friends}/accept"
+        const val RefuseFriendRequest = "${Friends}/refuse"
+        const val RequestList = "${Friends}/request"
     }
 }
