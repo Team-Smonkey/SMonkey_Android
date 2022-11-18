ca672dcb924b52da93284f687ab6f989c463c019
유현명 <aahspringaa4@naver.com>
2022-11-18 11:23:41 +0900

    ⚡️ :: Logout Implement

--- ui/src/main/java/com/example/smonkey_android/feature/setting/SettingFragment.kt
+++ ui/src/main/java/com/example/smonkey_android/feature/setting/SettingFragment.kt
@@ -1,18 +1,28 @@
 package com.example.smonkey_android.feature.setting
 
+import android.app.Activity
 import android.content.Intent
+import android.content.SharedPreferences
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.finishAffinity
+import androidx.preference.PreferenceManager
+import com.example.data.domain.token.Storage
+import com.example.data.domain.token.StorageImpl
 import com.example.smonkey_android.R
 import com.example.smonkey_android.base.BaseFragment
 import com.example.smonkey_android.databinding.FragmentSettingBinding
+import com.example.smonkey_android.feature.MainActivity
 import com.example.smonkey_android.feature.editauth.ChangeIdActivity
 import com.example.smonkey_android.feature.editauth.ChangePwActivity
 import com.example.smonkey_android.feature.health.HealthActivity
+import com.example.smonkey_android.feature.login.LoginActivity
 import com.example.smonkey_android.feature.monkeycolor.MonkeyColorActivity
 import com.example.smonkey_android.feature.mypage.MyPageActivity
 
 class SettingFragment : BaseFragment<FragmentSettingBinding>(
     R.layout.fragment_setting
 ) {
+
     override fun initView() {
 
         binding.moveMyProfile.setOnClickListener {
@@ -64,5 +74,15 @@ class SettingFragment : BaseFragment<FragmentSettingBinding>(
             val intent = Intent(activity, ChangePwActivity::class.java)
             startActivity(intent)
         }
+
+        binding.logout.setOnClickListener {
+            val sharedPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences()
+            val storage: Storage = StorageImpl(sharedPreferences)
+            storage.logout()
+            val intent = Intent(Activity(), LoginActivity::class.java)
+            finishAffinity(Activity());
+            startActivity(intent)
+        }
     }
+
 }
