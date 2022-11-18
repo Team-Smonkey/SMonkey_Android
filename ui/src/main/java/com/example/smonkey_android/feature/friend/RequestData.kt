a06710d3c7964650d930a68b718f619f9f6a90f3
유현명 <aahspringaa4@naver.com>
2022-11-18 11:19:41 +0900

    ⚡️ :: Friend Request List Data

---
+++ ui/src/main/java/com/example/smonkey_android/feature/friend/RequestData.kt
@@ -0,0 +1,26 @@
+package com.example.smonkey_android.feature.friend
+
+import com.example.data.remote.response.friend.RequestListResponse
+
+data class RequestData(
+    val friendId: Int,
+    val userName: String,
+    val smonkeyName: String,
+    val backgroundColor: String,
+    val step: Int,
+    val point: Int,
+    val level: Int,
+    val nextPoint: Int,
+)
+
+fun RequestListResponse.FriendList.toRv() =
+    RequestData(
+        friendId = friendId,
+        userName = userName,
+        smonkeyName = smonkeyName,
+        backgroundColor = backgroundColor,
+        step = step,
+        point = point,
+        level = level,
+        nextPoint = nextPoint,
+    )
