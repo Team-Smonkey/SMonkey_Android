1d7d5419db38faf31dcf86b77e491cae285858b7
유현명 <aahspringaa4@naver.com>
2022-11-18 11:17:43 +0900

    ⚡️ :: Friend List RecyclerView Data Class

---
+++ ui/src/main/java/com/example/smonkey_android/feature/friend/FriendListData.kt
@@ -0,0 +1,35 @@
+package com.example.smonkey_android.feature.friend
+
+import com.example.data.remote.response.friend.FriendListResponse
+import com.google.gson.annotations.SerializedName
+
+data class FriendListData(
+    val content: Content
+) {
+    data class Content(
+        val friendList: List<FriendList>
+    )
+
+    data class FriendList(
+        val friendId: Int,
+        val userName: String,
+        val smonkeyName: String,
+        val backgroundColor: String,
+        val step: Int,
+        val point: Int,
+        val level: Int,
+        val nextPoint: Int,
+    )
+}
+
+fun FriendListResponse.FriendList.toRv() =
+    FriendListData.FriendList(
+        friendId = friendId,
+        userName = userName,
+        smonkeyName = smonkeyName,
+        backgroundColor = backgroundColor,
+        step = step,
+        point = point,
+        level = level,
+        nextPoint = nextPoint
+    )
