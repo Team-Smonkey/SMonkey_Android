1fe5a19e5ce19ff36e75d3d9510ff958b12e56af
유현명 <aahspringaa4@naver.com>
2022-11-18 11:12:38 +0900

    ⚡️ :: Color Enum Type

---
+++ data/src/main/java/com/example/data/domain/enums/ColorType.kt
@@ -0,0 +1,26 @@
+package com.example.data.domain.enums
+
+enum class ColorType {
+    SeaGreen,
+    AppleGreen,
+    YellowGreen,
+    LightYellow,
+    LightOrange,
+    LightRed,
+    Cerise,
+    Magenta,
+    IndianRed,
+    LightCoral,
+    Bisqure,
+    Blossom,
+    Pink,
+    DarkPink,
+    Plum,
+    SkyBlue,
+    LightAqua,
+    LightBlue,
+    Violet,
+    Orchid,
+    Purple,
+    Navy,
+}
