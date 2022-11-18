0eed9d256a0f5e50d9a8d589e4d40c0da3869e2f
유현명 <aahspringaa4@naver.com>
2022-11-18 11:17:55 +0900

    ⚡️ :: Friend Adapter

---
+++ ui/src/main/java/com/example/smonkey_android/feature/friend/FriendAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.smonkey_android.feature.friend
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.smonkey_android.databinding.FriendItemBinding
+
+class FriendAdapter(
+    val data: List<FriendListData.FriendList>
+): RecyclerView.Adapter<FriendAdapter.ViewHolder>() {
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = data[position]
+
+        holder.bind(item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    class ViewHolder private constructor(val binding: FriendItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: FriendListData.FriendList) {
+            binding.namelevel.text = item.userName + "   " + item.level + "LEVEL"
+            binding.smonkeynamestep.text = item.smonkeyName + "   " + item.step + "단계"
+            binding.progress.progress = item.point
+            binding.progress.max = item.nextPoint
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = FriendItemBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+}
