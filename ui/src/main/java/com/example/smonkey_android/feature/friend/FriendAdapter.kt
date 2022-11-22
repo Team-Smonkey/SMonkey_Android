a48f1d142b8de01898ceb9a27e135410f7fe418a
유현명 <aahspringaa4@naver.com>
2022-11-22 11:52:17 +0900

    ⚡️ :: Change SMonkey Photo by Level

--- ui/src/main/java/com/example/smonkey_android/feature/friend/FriendAdapter.kt
+++ ui/src/main/java/com/example/smonkey_android/feature/friend/FriendAdapter.kt
@@ -3,6 +3,7 @@ package com.example.smonkey_android.feature.friend
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.smonkey_android.R
 import com.example.smonkey_android.databinding.FriendItemBinding
 
 class FriendAdapter(
@@ -27,6 +28,15 @@ class FriendAdapter(
             binding.smonkeynamestep.text = item.smonkeyName + "   " + item.step + "단계"
             binding.progress.progress = item.point
             binding.progress.max = item.nextPoint
+            if (item.level < 3) {
+                binding.ib.setImageResource(R.drawable.sickmonkey)
+            } else if (item.level < 9) {
+                binding.ib.setImageResource(R.drawable.tiredsmonkey)
+            } else if (item.level < 18) {
+                binding.ib.setImageResource(R.drawable.yummymonkey)
+            } else if (item.level < 30) {
+                binding.ib.setImageResource(R.drawable.happysmonkey)
+            }
             binding.executePendingBindings()
         }
 
